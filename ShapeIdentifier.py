731fe69508c1b4c9e3de1c191c24fc96efa36eb3
Talvin <43337507+TalvinJoshuaJacobs@users.noreply.github.com>
2019-04-10 17:25:35 +0100

    Update ShapeIdentifier.py

--- ShapeIdentifier.py
+++ ShapeIdentifier.py
@@ -1,14 +1,24 @@
+# A simple program that determines a simple shape given
+# three or four co-ordinates, and allows the user to scale
+# the shape and have it displayed on a graph using matplotlib
+# ==THIS FEATURE REQUIRES MATPLOTLIB TO BE INSTALLED ONTO THE VIEWING MACHINE==
+# Talvin Jacobs (C) 2019
+
+#Imports
 import math
 import matplotlib.pyplot as plt
 
 print ("Welcome to the Shape Identifier Program!")
 print ("")
 
+#Get Co-ord number input
 coordNumber = input ("Will you be entering 3 or 4 co-ordinates? (Please enter an integer response) ")
 print ("")
 
+#While a correct input is entered
 while coordNumber == "3" or "4":
-    
+
+    #Get the co-ordinates from the user
     if coordNumber == "3":
         threeInputXOne = int(input ("Please enter the x value of the first co-ordinate "))
         threeInputYOne = int(input ("Please enter the y value of the first co-ordinate "))
@@ -19,22 +29,45 @@ while coordNumber == "3" or "4":
         threeInputXThree = int(input ("Please enter the x value of the third co-ordinate "))
         threeInputYThree = int(input ("Please enter the y value of the third co-ordinate "))
         print ("")
-        
+
+        #Display the co-ordinates in regular format
         print ("Here are the co-ordinates you have entered:")
         print ("1: (",threeInputXOne,",",threeInputYOne,")")
         print ("2: (",threeInputXTwo,",",threeInputYTwo,")")
         print ("3: (",threeInputXThree,",",threeInputYThree,")")
         print ("")
 
+        #Display the co-ordinates in vector format
         print ("In vector format they are:")
         print ("1: ",threeInputXOne,"i + ",threeInputYOne,"j")
         print ("2: ",threeInputXTwo,"i + ",threeInputYTwo,"j")
         print ("3: ",threeInputXThree,"i + ",threeInputYThree,"j")
         print ("")
         print ("This shape is a triangle!")
+
+        #Allow the user the option to scale their entered shape
+        Scale = int(input ("What scale factor would you like to scale the shape to? "))
+
+        #Creating new scaling variables
+        threeInputXOneNew = threeInputXOne * Scale
+        threeInputXTwoNew = threeInputXTwo * Scale
+        threeInputXThreeNew = threeInputXThree * Scale
+        threeInputYOneNew = threeInputYOne * Scale
+        threeInputYTwoNew = threeInputYTwo * Scale
+        threeInputYThreeNew = threeInputYThree * Scale
+
+        #Using the new variables and matplotlib to display the final result
+        print ("")
+        print ("Here is a graph displaying the co-ordinates you entered:")
+        UserXCoord = [threeInputXOneNew, threeInputXTwoNew, threeInputXThreeNew]
+        UserYCoord = [threeInputYOneNew, threeInputYTwoNew, threeInputYThreeNew]
+        plt.plot(UserXCoord, UserYCoord)
+        plt.show()
+        print ("")
         
         break
-    
+
+    #Get the coordinates from the user
     elif coordNumber == "4":
         fourInputXOne = int(input ("Please enter the x value of the first co-ordinate "))
         fourInputYOne = int(input ("Please enter the y value of the first co-ordinate "))
@@ -49,12 +82,14 @@ while coordNumber == "3" or "4":
         fourInputYFour = int(input ("Please enter the y value of the fourth co-ordinate "))
         print ("")
 
+        #Display them in standard format
         print ("Here are the co-ordinates you have entered:")
         print ("1: (",fourInputXOne,",",fourInputYOne,")")
         print ("2: (",fourInputXTwo,",",fourInputYTwo,")")
         print ("3: (",fourInputXThree,",",fourInputYThree,")")
         print ("4: (",fourInputXFour,",",fourInputYFour,")")
 
+        #Display the coordinates in vector format
         print ("")
         print ("In vector format they are:")
         print ("1: ",fourInputXOne,"i + ",fourInputYOne,"j")
@@ -63,11 +98,13 @@ while coordNumber == "3" or "4":
         print ("4: ",fourInputXFour,"i + ",fourInputYFour,"j")
         print ("")
 
+        #Determining the length of each side
         sideOneLength = math.sqrt((fourInputXTwo-fourInputXOne)**2 + (fourInputYTwo-fourInputYOne)**2 )
         sideTwoLength = math.sqrt((fourInputXThree-fourInputXTwo)**2 + (fourInputYThree-fourInputYTwo)**2 )
         sideThreeLength = math.sqrt((fourInputXFour-fourInputXThree)**2 + (fourInputYFour-fourInputYThree)**2 )
         sideFourLength = math.sqrt((fourInputXOne-fourInputXFour)**2 + (fourInputYOne-fourInputYFour)**2 )
 
+        #Setting variables for the definintion of the angles
         AOneFirstVectI = fourInputXOne-fourInputXFour
         AOneFirstVectJ = fourInputYOne-fourInputYFour
         AOneSecondVectI = fourInputXOne-fourInputXTwo
@@ -88,6 +125,7 @@ while coordNumber == "3" or "4":
         AFourSecondVectI = fourInputXFour-fourInputXOne
         AFourSecondVectJ = fourInputYFour-fourInputYOne
 
+        #Calculating the angles using the previously set variables
         AOneMagOne = math.sqrt((AOneFirstVectI)**2 + (AOneFirstVectJ)**2 )
         AOneMagTwo = math.sqrt((AOneSecondVectI)**2 + (AOneSecondVectJ)**2 )
         AOneDotProduct = AOneFirstVectI*AOneSecondVectI + AOneFirstVectJ*AOneSecondVectJ
@@ -108,6 +146,7 @@ while coordNumber == "3" or "4":
         AFourDotProduct = AFourFirstVectI*AFourSecondVectI + AFourFirstVectJ*AFourSecondVectJ
         AngleFour = (math.acos(AFourDotProduct/(AFourMagOne*AFourMagTwo))*180)/math.pi
 
+        #Displaying the calculated angles
         print ("The angle sizes are:")
         print ("Angle 1: ",AngleOne)
         print ("Angle 2: ",AngleTwo)
@@ -115,6 +154,7 @@ while coordNumber == "3" or "4":
         print ("Angle 4: ",AngleFour)
         print ("")
 
+        #Displaying the lengths of each side
         print ("The lengths of each side are:")
         print ("Length 1: ",sideOneLength)
         print ("Length 2: ",sideTwoLength)
@@ -122,6 +162,7 @@ while coordNumber == "3" or "4":
         print ("Length 4: ",sideFourLength)
         print ("")
 
+        #Determining the shape using the parameters calculated
         if AngleOne == AngleTwo == AngleThree == AngleFour:
             if sideOneLength == sideTwoLength == sideThreeLength == sideFourLength:
                 print ("This shape is a square")
@@ -130,20 +171,31 @@ while coordNumber == "3" or "4":
         else:
             print ("This shape is neither a square triangle or rectangle")
 
-        print ("")
+        #Allowing the user to scale the shape
+        Scale = int(input ("What scale factor would you like to scale the shape to? "))
 
-        UserXCoord = [fourInputXOne, fourInputXTwo, fourInputXThree, fourInputXFour]
-        UserYCoord = [fourInputYOne, fourInputYTwo, fourInputYThree, fourInputYFour]
+        #New shape scaling parameters
+        fourInputXOneNew = fourInputXOne * Scale
+        fourInputXTwoNew = fourInputXTwo * Scale
+        fourInputXThreeNew = fourInputXThree * Scale
+        fourInputXFourNew = fourInputXFour * Scale
+        fourInputYOneNew = fourInputYOne * Scale
+        fourInputYTwoNew = fourInputYTwo * Scale
+        fourInputYThreeNew = fourInputYThree * Scale
+        fourInputYFourNew = fourInputYFour * Scale
+
+        #Plotting the shape on a visual display
+        print ("")
+        print ("Here is a graph displaying the co-ordinates you entered:")
+        UserXCoord = [fourInputXOneNew, fourInputXTwoNew, fourInputXThreeNew, fourInputXFourNew]
+        UserYCoord = [fourInputYOneNew, fourInputYTwoNew, fourInputYThreeNew, fourInputYFourNew]
         plt.plot(UserXCoord, UserYCoord)
         plt.show()
+        print ("")
         
         break
+    
+    #If the user has not entered a valid input for the number of co-ordinates to be entered
     else:
         print ("Please try again")
         coordNumber = input ("Will you be entering 3 or 4 co-ordinates? ")
-
-
-
-
-        
-    
